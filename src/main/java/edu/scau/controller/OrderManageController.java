74c2ea3787931c79b2b99742bb24f989aada048c
EasonChung <yszhong95@vip.qq.com>
2017-07-05 17:28:18 +0800

    订单 - 管理员：做了一半

---
+++ src/main/java/edu/scau/controller/OrderManageController.java
@@ -0,0 +1,29 @@
+package edu.scau.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.scau.service.OrderManageService;
+
+@Controller
+@RequestMapping("/ordermgr")
+public class OrderManageController {
+
+	@Autowired
+	private OrderManageService service;
+	
+	public void list(HttpServletResponse response){
+		String json = service.list();
+		try {
+			response.getWriter().println(json);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
